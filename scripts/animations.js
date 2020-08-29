2a9a7cba71d7960ada2c633d99aaa3f9a856b5f3
MartinYe1234 <martinye022@gmail.com>
2020-08-28 18:35:28 -0700

    gif

--- scripts/animations.js
+++ scripts/animations.js
@@ -5,12 +5,12 @@ function test(){
 
 function hoverOver(){
 	if (document.getElementsByClassName("projectSnake")){
-		document.getElementsByClassName("projectSnake").src="images/StewburryLOGO.png";
+		document.getElementsById("snakeLOGO").src="images/snakeGameGIF.gif";
 	}
 }
 
 function hoverOff(){
 	if (document.getElementsByClassName("projectSnake")){
-		document.getElementsByClassName("projectSnake").src="images/snakeGameLOGO.png";
+		document.getElementsById("snakeLOGO").src="images/snakeGameLOGO.png";
 	}
 }
