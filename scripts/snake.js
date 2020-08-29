010a284883445f1b0e7afd4b8915a41cce3d2890
MartinYe1234 <martinye022@gmail.com>
2020-08-28 19:03:50 -0700

    Update snake.js

--- scripts/snake.js
+++ scripts/snake.js
@@ -7,8 +7,16 @@ var gameArena = {
 		this.context = this.canvas.getContext("2d");
 		document.body.insertBefore(this.canvas, null); //inserts the canvas at the bottom of the page
 	}
+	clear : function(){
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	}
 }
 // function used to initiliaze canvas on page load
 function startGame(){
 	gameArena.start();
+}
+
+// our draw function that constantly updates our canvas
+function draw(){
+	canvas
 }
