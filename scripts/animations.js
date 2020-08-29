d598fa07012790c97d28f9bf04965249e7e407f5
MartinYe1234 <martinye022@gmail.com>
2020-08-28 17:56:47 -0700

    gif on hover

--- scripts/animations.js
+++ scripts/animations.js
@@ -2,3 +2,9 @@ var testvar = "green";
 function test(){
 	document.getElementById("header").style.color = testvar;
 }
+function hoverOver(){
+	if (document.getElementsByClassName("projectSnake")){
+		test();
+	}
+	
+}
