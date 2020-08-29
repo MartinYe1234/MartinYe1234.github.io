a84e67ca0b508e19cc6d7eda98c2647fcc56ad68
MartinYe1234 <martinye022@gmail.com>
2020-08-28 18:11:19 -0700

    test

--- scripts/animations.js
+++ scripts/animations.js
@@ -2,9 +2,15 @@ var testvar = "green";
 function test(){
 	document.getElementById("header").style.color = testvar;
 }
+
 function hoverOver(){
 	if (document.getElementsByClassName("projectSnake")){
-		test();
+		this.src="images/StewburryLOGO.png";
 	}
-	
+}
+
+function hoverOff(){
+	if (document.getElementsByClassName("projectSnake")){
+		this.src="images/snakeGameLOGO.png";
+	}
 }
