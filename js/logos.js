10515c774f2a1a4c8a6ebd74f4f49cf0f1e49346
MartinYe1234 <martinye022@gmail.com>
2020-09-06 22:00:42 -0700

    graph Algos

--- js/logos.js
+++ js/logos.js
@@ -7,10 +7,16 @@ function hoverOver(){
 	if (document.getElementById("snakeLOGO")){
 		document.getElementById("snakeLOGO").src="images/snakeGameGIF.gif";
 	}
+	else if (document.getElementById("graphLOGO")){
+		document.getElementById("graphLOGO").src="images/graphGIF.gif";
+	}
 }
 
 function hoverOff(){
 	if (document.getElementById("snakeLOGO")){
 		document.getElementById("snakeLOGO").src="images/snakeGameLOGO.png";
+	}
+	else if (document.getElementById("graphLOGO")){
+		document.getElementById("graphLOGO").src="images/graphLOGO.png";
 	}
 }
