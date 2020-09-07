fa17e35477d55678229be26162feb876f525ca0b
MartinYe1234 <martinye022@gmail.com>
2020-09-07 10:35:05 -0700

    various things

--- js/graphClasses.js
+++ js/graphClasses.js
@@ -7,17 +7,29 @@ class Graph{
 		// Adds a node to the graph
 		this.graph[node] = [];
 	}
+	delEdge(edge){ // unfinished
+		let index = this.edge_list.indexOf(edge);
+		this.edge_list.splice(edge, 1);
+	}
 }
 class Node{
-	constructor(name, colour, position, state){
-		this.name = name
-		this.colour = colour
-		this.position = position
-		this.state = state
+	constructor(name, pos, state){
+		this.name = name;
+		this.position = pos;
+		this.state = state;
 	}
 }
 class Edge{
-	constructor(){
-		
+	constructor(u, v){
+		this.u = u;
+		this.v = v;
+		let x1 = this.u.pos[0], y1 = this.u.pos[1], x2 = this.v.pos[0], y2 = this.v.pos[1];
+		this.weight = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5;
+	}
+	get_edge(){
+		return this;
+	}
+	get_edge_data(){
+		return [this.u.name, this.v.name, this.weight];
 	}
 }
--- js/graphInterface.js
+++ js/graphInterface.js
@@ -1,1 +1,21 @@
-var test = 1;
+var canvas;
+var context;
+var mode;
+
+function initialiseScreen() {
+    canvas = document.getElementById("game");
+    context = canvas.getContext("2d");
+    interval = setInterval(draw, 200);
+    context.canvas.width  = window.innerWidth;
+    context.canvas.height = window.innerHeight;
+}
+
+// our draw function that constantly updates our canvas
+function draw() {
+    //clear screen everytime
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function toggleMode(){
+    
+}
