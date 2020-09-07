9a6362ef1c00911d9647abfad0126f2b2d19920e
MartinYe1234 <martinye022@gmail.com>
2020-09-06 22:13:54 -0700

    changed file names

--- js/algos.js
+++
@@ -1,5 +0,0 @@
-class Graph{
-	constructor(){
-		
-	}
-}
---
+++ js/graphClasses.js
@@ -0,0 +1,19 @@
+class Graph{
+	constructor(){
+		this.graph = {}
+		this.edge_list = []
+	}
+	addNode(node){
+		// Adds a node to the graph
+		this.graph[node] = [];
+	}
+}
+class Node{
+	constructor(){
+	}
+}
+class Edge{
+	constructor(){
+		
+	}
+}
