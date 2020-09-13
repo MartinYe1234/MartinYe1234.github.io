448df48e9c515f0200ad71da8a70d93ab5e514e1
MartinYe1234 <martinye022@gmail.com>
2020-09-13 11:35:01 -0700

    Update graphAlgos.js
    bfs

--- js/graphAlgos.js
+++ js/graphAlgos.js
@@ -1,3 +1,19 @@
-function bfs(graph){ // runs bfs and recieves graph as an adjacency list
-    
+function bfs(graph, start){ // runs bfs and recieves graph as an adjacency list
+    let order_visited = [start]; // used for the animation of bfs, contains pairs of nodes, in order that they were visited
+    let queue = [start];
+    let nodes = Object.keys(graph.graph);
+    let visited = {}; // visited dictionary
+    nodes.forEach(function(node){visited[node] = 0});
+    while(queue.length != 0){
+        let current = queue.shift(); // dequeue
+        visited[current] = 1;// mark as visited
+        graph[current].forEach(function(adjacent){
+            let adjacent_node = adjacent[0];
+            if(!visited[adjacent_node]){
+                queue.push(adjacent_node);
+                order_visited.push([current, adjacent_node]);
+            }
+        });   
+    }
+    return order_visited;
 }
